22744d469a69b7d43d27216c732a5512220299df
unfreezed <168965126+unfreezed@users.noreply.github.com>
2024-05-05 13:39:02 +0300

    Update podkryoi.py

--- podkryoi.py
+++ podkryoi.py
@@ -1,3 +1,19 @@
+#    Friendly Telegram (telegram userbot)
+#    Copyright (C) 2018-2019 The Authors
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import asyncio
 import logging
 
@@ -6,6 +22,7 @@ from telethon.tl.types import InputMediaDice
 from .. import loader, utils, security
 
 logger = logging.getLogger(__name__)
+
 
 @loader.tds
 class DiceMod(loader.Module):
@@ -22,32 +39,46 @@ class DiceMod(loader.Module):
     @loader.unrestricted
     async def dicecmd(self, message):
         """Rolls a die (optionally with the specified value)
-           .dice <emoji> <count>"""
+           .dice <emoji> <outcomes> <count>"""
         args = utils.get_args(message)
         if await self.allmodules.check_security(message, security.OWNER | security.SUDO):
             try:
                 emoji = args[0]
             except IndexError:
                 emoji = "🎲"
-
             possible = self.config["POSSIBLE_VALUES"].get(emoji, None)
             if possible is None:
                 emoji = "🎲"
-            
+                possible = self.config["POSSIBLE_VALUES"][emoji]
+            values = set()
             try:
-                count = int(args[1])
+                for val in args[1].split(","):
+                    value = int(val)
+                    if value in possible:
+                        values.add(value)
+            except (ValueError, IndexError):
+                values.clear()
+            try:
+                count = int(args[2])
             except (ValueError, IndexError):
                 count = 1
-
+            rolled = -1
+            done = 0
             chat = message.to_id
             client = message.client
-            task = None
-
-            for _ in range(count):
+            while True:
                 task = client.send_message(chat, file=InputMediaDice(emoji))
-
-            if task:
-                await asyncio.gather(message.delete(), task)
+                if message:
+                    message = (await asyncio.gather(message.delete(), task))[1]
+                else:
+                    message = await task
+                rolled = message.media.value
+                logger.debug("Rolled %d", rolled)
+                if rolled in values or not values:
+                    done += 1
+                    message = None
+                    if done == count:
+                        break
         else:
             try:
                 emoji = args[0]
